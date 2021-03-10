332351ad7318075c92e2b2241a8f25bd3dc0ba59
Anil1 <anil1@gamil.com>
2021-03-10 16:55:40 +0530

    changes made to april rel

--- Simple.java
+++ Simple.java
@@ -1,6 +1,7 @@
 class Simple{  
-    public static void main(String args[]){  
+    public static void main(String args[]){
+	    int a, b, c;
      System.out.println("Hello Java");
-     System.out.println("Hi");
+     System.out.println("This is my april-release code");
     }  
 }  
